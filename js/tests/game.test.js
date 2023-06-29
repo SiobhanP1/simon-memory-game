caf1cbcbda2acccee65cfae40754218b77e73b33
SiobhanP1 <siobhanphillips11@gmail.com>
2023-06-29 08:53:35 +0000

    Add tests to check that choices, playerMoves and currentGame keys exist in game object

--- js/tests/game.test.js
+++ js/tests/game.test.js
@@ -15,5 +15,14 @@ beforeAll(()=> {
 describe("game object contains", ()=> {
     test("score key exists", ()=> {
         expect("score" in game).toBe(true);
-    })
+    });
+    test("currentGame key exists", ()=> {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", ()=> {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", ()=> {
+        expect("choices" in game).toBe(true);
+    });
 })
